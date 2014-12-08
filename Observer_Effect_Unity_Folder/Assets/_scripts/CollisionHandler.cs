8481bc315a559177b95c89425e5d256627b415c1
Boombostak <smokejaguar3050@hotmail.com>
2014-12-08 13:56:44 -0700

    Collisions now work
    -Needed to reference gameObject.name instead of the gameObject itself
    (which is probably just an ID code).

--- Observer_Effect_Unity_Folder/Assets/_scripts/CollisionHandler.cs
+++ Observer_Effect_Unity_Folder/Assets/_scripts/CollisionHandler.cs
@@ -20,7 +20,7 @@ public class CollisionHandler : MonoBehaviour {
     void OnTriggerEnter (Collider other)
     {
         Debug.Log("touch");
-        if (other == collides_with.collider)
+        if (other.gameObject.name == collides_with.name)
         {
             Instantiate(spawns);
             Destroy (other.gameObject);
