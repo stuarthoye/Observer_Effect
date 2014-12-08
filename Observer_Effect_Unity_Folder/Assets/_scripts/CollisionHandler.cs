3c20966094f818f662a0f03995c8ef6a38e423df
Boombostak <smokejaguar3050@hotmail.com>
2014-12-08 13:28:32 -0700

    Added CollisionHandler script

---
+++ Observer_Effect_Unity_Folder/Assets/_scripts/CollisionHandler.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionHandler : MonoBehaviour {
+
+    public GameObject collides_with;
+    public GameObject spawns;
+    public Collision collision;
+
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        Debug.Log("touch");
+        if (other == collides_with.collider)
+        {
+            Instantiate(spawns);
+            Destroy (other.gameObject);
+            Destroy(this);
+        }
+    }
+}
