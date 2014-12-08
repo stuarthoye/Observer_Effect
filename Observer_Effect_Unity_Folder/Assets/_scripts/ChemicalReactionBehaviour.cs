93d93971f11a10533b4005e82ea016e7691743a0
Boombostak <smokejaguar3050@hotmail.com>
2014-12-07 22:22:34 -0700

    Roughed in Purple spawner
    -Event-based Instantiate/Destroy.
    -Particles are offset in a weird way.

---
+++ Observer_Effect_Unity_Folder/Assets/_scripts/ChemicalReactionBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChemicalReactionBehaviour : MonoBehaviour {
+
+    public GameObject thing_to_spawn;
+    
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+
+    void SpawnParticle()
+    {
+        Instantiate(thing_to_spawn);
+    }
+}
