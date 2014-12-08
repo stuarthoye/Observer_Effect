f7d3111cfe5e073a864d207ce11ff4420d8930bf
Boombostak <smokejaguar3050@hotmail.com>
2014-12-07 23:10:39 -0700

    Title screen
    -Needs music.
    -All events handled in Canvas animation.

---
+++ Observer_Effect_Unity_Folder/Assets/_scripts/Title_Screen_Behaviour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Title_Screen_Behaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void AdvanceToFirstLevel()
+    {
+        Application.LoadLevel(Application.loadedLevel + 1);
+    }
+}
