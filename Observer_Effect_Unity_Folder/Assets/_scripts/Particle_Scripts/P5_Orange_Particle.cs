73b13333f7911c8587575d10b75e5ae332716885
Stuart Hoye <stuarthoye@gmail.com>
2014-12-24 06:24:21 -0700

    Orange Particle
    - Orange Particle is moving.

--- Observer_Effect_Unity_Folder/Assets/_scripts/Particle_Scripts/P5_Orange_Particle.cs
+++ Observer_Effect_Unity_Folder/Assets/_scripts/Particle_Scripts/P5_Orange_Particle.cs
@@ -2,14 +2,59 @@
 using System.Collections;
 
 public class P5_Orange_Particle : MonoBehaviour {
+	public float distance = 5;
+	public bool visible;
+	
+	private Vector3 start;
+	private Vector3 end;
+	private float scalar = 0;
+	private bool outgoing = true;
+	private int counter = 0;
 
 	// Use this for initialization
 	void Start () {
-	
+		end = transform.position - (transform.forward * distance);
+		start = transform.position;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
+		visible = renderer.isVisible;
+		if (visible) {
+			Oscillate();
+		}
+	}
+
+	void Oscillate (){
+		scalar += Time.deltaTime;
+		float scale_amt = Time.deltaTime;
+		
+		if (outgoing){
+			transform.position = Vector3.Lerp (start, end, scalar);
+			transform.localScale += new Vector3(scale_amt, scale_amt, scale_amt);
+		} else {
+			transform.position = Vector3.Lerp (end, start, scalar);
+			transform.localScale -= new Vector3(scale_amt, scale_amt, scale_amt);
+		}
+		if (transform.position == end || transform.position == start){
+			outgoing = !outgoing;
+			scalar = 0;
+			if (transform.position == start){
+				Update_Targets();
+			}
+		}
+	}
 	
+	// This function rotates the particle once it reaches its endpoint.
+	void Update_Targets(){
+		counter++;
+		if (counter == 3){
+			counter = 0;
+			transform.Rotate(270, 180, 0);
+		} else {
+			transform.Rotate (90, 90, 0);
+		}
+		end = transform.position - (transform.forward * distance);
+		start = transform.position;
 	}
 }
